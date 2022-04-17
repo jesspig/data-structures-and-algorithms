33322a4cd968c7659b7e8cb2486e4d0a27b8d19e
JessPig <jesspig552@gmail.com>
2022-04-17 23:29:45 +0800

    ADD Java Singly-Linked-List

---
+++ linked-list/Java/README.md
@@ -0,0 +1,9 @@
+# 数结构与算法 - 链表 (C)
+
+|   目录   |                             文件                             |
+| :------: | :----------------------------------------------------------: |
+| 链表介绍 | [README.md](https://github.com/jesspig/data-structures-and-algorithms/blob/main/linked-list/README.md) |
+|  单链表  | [SinglyLinkedList.java](https://github.com/jesspig/data-structures-and-algorithms/blob/main/linked-list/Java/SinglyLinkedList.java) |
+
+参考文章：[数据结构与算法 - 单链表](http://jesspig.github.io/blog/2022/04/17/184139/9b23354010ea/)
+
---
+++ linked-list/Java/SinglyLinkedList.java
@@ -0,0 +1,132 @@
+class Node {
+  public char val;
+  public Node next;
+
+  public Node() {
+  }
+
+  @Override
+  public String toString() {
+    return "Node{" +
+        "val=" + val +
+        ", next=" + next +
+        '}';
+  }
+}
+
+public class SinglyLinkedList {
+
+  public Node Init() {
+    Node head = new Node();
+    head.next = null;
+    System.out.println("Init success");
+    return head;
+  }
+
+  public void InsertHead(Node head, char val) {
+    Node p = new Node();
+    p.val = val;
+    p.next = head.next;
+    head.next = p;
+    System.out.println("InsertHead success");
+  }
+
+  public void InsertTail(Node head, char val) {
+    Node p = new Node();
+    p.val = val;
+    p.next = null;
+    if (head.next == null)
+      head.next = p;
+    else {
+      Node q = head.next;
+      while (q.next != null)
+        q = q.next;
+      q.next = p;
+    }
+    System.out.println("InsertTail success");
+  }
+
+  public void InsertMiddle(Node head, char val, int idx) {
+    Node t = head;
+    for (int i = 0; i < idx; i++) {
+      t = t.next;
+      if (t.next == null)
+        System.out.println("insert position error");
+    }
+
+    Node p = new Node();
+    p.val = val;
+    p.next = t.next;
+    t.next = p;
+    System.out.println("InsertMiddle success");
+  }
+
+  public void DeleteHead(Node head) {
+    if (head.next == null) {
+      System.out.println("head is empty");
+      return;
+    }
+    Node p = head.next;
+    head.next = p.next;
+    System.out.println("DeleteHead success");
+  }
+
+  public void DeleteTail(Node head) {
+    if (head.next == null) {
+      System.out.println("head is empty");
+      return;
+    }
+    Node p = head.next, q = null;
+    while (p.next != null) {
+      q = p;
+      p = p.next;
+    }
+    q.next = null;
+    System.out.println("DeleteTail success");
+  }
+
+  public void DeleteMiddle(Node head, int idx) {
+    Node p = head.next, q = null;
+    for (int i = 0; i < idx; i++) {
+      q = p;
+      p = p.next;
+      if (p == null) {
+        System.out.println("delete position error");
+        return;
+      }
+    }
+    q.next = p.next;
+    System.out.println("DeleteMiddle success");
+  }
+
+  public void Modify(Node head, int idx, char val) {
+    Node p = head;
+    for (int i = 0; i < idx; i++) {
+      p = p.next;
+      if (p == null)
+        System.out.println("modify position error");
+    }
+    p.val = val;
+    System.out.println("Modify success");
+  }
+
+  public void Display(Node head) {
+    Node p = head.next;
+    while (p != null) {
+      System.out.print(p.val);
+      p = p.next;
+    }
+    System.out.println();
+  }
+
+  public void Destroy(Node head) {
+    Node p = null;
+    while (head.next != null) {
+      p = head.next;
+      head.next = p.next;
+      p = null;
+    }
+    System.out.println("Destroy success");
+  }
+
+}
